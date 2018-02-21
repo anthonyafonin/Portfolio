1bf2c3b7a6cfe7018ec7179ab303a05b7f46b3ec
anthonyafonin <anthonyafonin@gmail.com>
2018-02-20 23:31:51 -0800

    deploy

---
+++ portfolio-old/projects.php
@@ -0,0 +1,88 @@
+<?php include 'includes/header.php';?>
+
+<!-- Banner -->
+<section id="banner">
+	<div class="inner">
+		<h2>Projects</h2>
+	</div>
+</section>
+
+<!-- One -->
+<section id="one" class="wrapper style1">
+	<header class="major">
+		<h2>My Work</h2>
+		<p>Launch or view the source files of my larger projects.</p>
+	</header>
+	<div class="container">
+		<div class="row">
+			<div class="4u">
+				<section class="special box">
+					<i class="icon fa fa-database major"></i>
+                    <ul class="limitless">
+                        <li><a href="Projects/LimitlessDatabase/index.php" target="_blank">Limitless DB</a></li>
+                    </ul>
+
+					<p>A websire that uses PHP and MySQL to assist a construction company in managing their assets.</p>
+                    <ul class="actions">
+                        <li><a href="https://github.com/anthonyafonin/LimitlessDB" target="_blank" class="button alt">Source</a></li>
+					</ul>
+				</section>
+			</div>
+			<div class="4u">
+				<section class="special box">
+					<i class="icon fa fa-android major"></i>
+					<h3>Quick Sheets</h3>
+					<p>An Android Application that allows the user to create time sheets that are stored on a local SQlite database.</p>
+                    <ul class="actions">
+						<li><a href="https://github.com/anthonyafonin/QuickSheets" target="_blank" class="button alt">Source</a></li>
+					</ul>
+				</section>
+			</div>
+			<div class="4u">
+				<section class="special box">
+					<i class="icon fa fa-heartbeat major"></i>
+					<h3>Health Calc</h3>
+					<p>A Health Application made with Visual Basic that calculates BMI, BMR, and other health factors by using the User's input.</p>
+                    <ul class="actions">
+						<li><a href="https://github.com/anthonyafonin/HealthCalc" target="_blank" class="button alt">Source</a></li>
+					</ul>
+				</section>
+			</div>
+		</div>
+        <div class="container">
+		<div class="row">
+			<div class="4u">
+				<section class="special box">
+					<i class="icon fa fa-book major"></i>
+					<h3>MadLibs</h3>
+					<p>A JavaFX application that tells 3 MadLib stories with the FreeTTS voice API.</p>
+                    <ul class="actions">
+						<li><a href="https://github.com/anthonyafonin/MadLibs" target="_blank" class="button alt">Source</a></li>
+					</ul>
+				</section>
+			</div>
+			<div class="4u">
+				<section class="special box">
+					<i class="icon fa fa-cube major"></i>
+					<h3>Threaded Dice</h3>
+					<p>A JavaFX application that counts the results of two rolled die using threads.</p>
+                    <ul class="actions">
+						<li><a href="https://github.com/anthonyafonin/ThreadedDice" target="_blank" class="button alt">Source</a></li>
+					</ul>
+				</section>
+			</div>
+			<div class="4u">
+				<section class="special box">
+					<i class="icon fa fa-calculator major"></i>
+					<h3>Calculator</h3>
+					<p>A Java Calculator application that demonstrates bridged classes.</p>
+                    <ul class="actions">
+						<li><a href="https://github.com/anthonyafonin/Calculator" target="_blank"  class="button alt">Source</a></li>
+					</ul>
+				</section>
+			</div>
+		</div>
+	</div>
+</section>
+
+<?php include 'includes/footer.php';?>
